1c53ba865b02070a80f84997fd6d5305f1015b50
Doug Wilson <d@dkw.io>
2016-06-02 08:55:59 -0700

    Initial commit

---
+++ src/DKW.AspNet.StructureMap/IoC.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace DKW.AspNet.StructureMap
+{
+	public class IoC
+	{
+		/// <summary>
+		/// Configures ASP.NET MVC and ASP.NET WebAPI to use StructureMap as the Dependency Resolver.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="config"></param>
+		public static void Bootstrap<T>(HttpConfiguration config)
+				where T : Registry, new()
+		{
+			Configure(config, new T());
+		}
+
+		/// <summary>
+		/// Configures ASP.NET MVC and ASP.NET WebAPI to use StructureMap as the Dependency Resolver.
+		/// </summary>
+		/// <param name="config"></param>
+		/// <param name="registry"></param>
+		public static void Configure(HttpConfiguration config, Registry registry)
+		{
+			var container = new Container(registry);
+			var scope = new StructureMapScope(container);
+
+			DependencyResolver.SetResolver(scope);
+			config.DependencyResolver = scope.GetResolver();
+		}
+	}
+}
---
+++ src/DKW.AspNet.StructureMap/StructureMapResolver.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http.Dependencies;
+using StructureMap;
+
+namespace DKW.AspNet.StructureMap
+{
+    public class StructureMapResolver : StructureMapScope, IDependencyResolver
+    {
+        public StructureMapResolver(IContainer container) : base(container)
+        {
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return GetChildScope();
+        }
+    }
+}
