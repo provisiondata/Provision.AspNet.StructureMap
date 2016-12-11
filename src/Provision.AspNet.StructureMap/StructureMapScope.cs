6a47af66eeb691966ff717abcbc537d1045990c2
Doug Wilson <d@dkw.io>
2016-12-11 12:29:41 -0800

    Remove DEBUG conditional code

--- src/Provision.AspNet.StructureMap/StructureMapScope.cs
+++ src/Provision.AspNet.StructureMap/StructureMapScope.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using System.Web.Http.Dependencies;
 using Microsoft.Practices.ServiceLocation;
 using StructureMap;
@@ -11,24 +9,11 @@ namespace Provision.AspNet.StructureMap
 {
 	public class StructureMapScope : IDependencyScope, IServiceLocator
 	{
-#if DEBUG
-		private static Int32 _instanceCount;
-		private static Int32 _disposedCount;
-		private Int32 _instanceNumber;
-#endif
 		private Boolean _disposed;
-		private IContainer _container;
+		private readonly IContainer _container;
 
 		public StructureMapScope(IContainer container)
 		{
-#if DEBUG
-			_instanceNumber = Interlocked.Increment(ref _instanceCount);
-			if (_instanceNumber == 1) {
-				Debug.WriteLine(container.WhatDidIScan());
-				Debug.WriteLine(container.WhatDoIHave());
-			}
-			Debug.WriteLine($"StructureMapScope Instantiated - Instance: {_instanceNumber}  Created: {_instanceCount}  Disposed: {_disposedCount}");
-#endif
 			_container = container;
 		}
 
@@ -86,34 +71,21 @@ namespace Provision.AspNet.StructureMap
 		{
 			if (!_disposed) {
 				if (disposing) {
-					// TODO: dispose managed state (managed objects).
+					// Dispose managed state
 					_container.Dispose();
 				}
 
-				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-				// TODO: set large fields to null.
+				// Free unmanaged resources
+				// Set large fields to null.
 
 				_disposed = true;
-#if DEBUG
-				Interlocked.Increment(ref _disposedCount);
-				Debug.WriteLine($"StructureMapScope Disposed - Instance: {_instanceNumber}  Created: {_instanceCount}  Disposed: {_disposedCount}");
-#endif
 			}
 		}
 
-		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-		// ~Scope() {
-		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-		//   Dispose(false);
-		// }
-
-		// This code added to correctly implement the disposable pattern.
 		public void Dispose()
 		{
 			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
 			Dispose(true);
-			// TODO: uncomment the following line if the finalizer is overridden above.
-			// GC.SuppressFinalize(this);
 		}
 	}
 }
